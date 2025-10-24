d7bbc61209d10e0e5ce9dfdc6e1a2efc9c6783d6
denis | walderhu <denis.lipatov163@gmail.com>
2025-10-24 15:08:05 +0300

    пока конечная версия, но он так и крутится в одну сторону 1 и в обратную 2

--- src/file.py
+++ src/file.py
@@ -22,10 +22,10 @@ class Stepper:
         return int(clock_div)
 
     def __init__(self, dir_pin, en_pin, step_pin, invert_en=False, clock_div=80):
+        self.invert_en = invert_en
         self.dir_pin = Pin(dir_pin, Pin.OUT)
         self.rmt = esp32.RMT(self.id, pin=Pin(step_pin, Pin.OUT), clock_div=clock_div)  
         self.en_pin = Pin(en_pin, Pin.OUT)
-        self.invert_en = invert_en
 
     def enable(self, state=True):
         if self.invert_en: state = not state
@@ -39,25 +39,36 @@ class Stepper:
         self.enable(False)
         return False
 
-    def move(self, steps, step_delay_us=500, delay=50):
-        self.dir_pin.value(steps > 0); steps = abs(steps)
+    def move(self, steps, step_delay_us=500, batch_size=200):
+        direction = steps > 0; steps = abs(steps)
+        self.dir_pin.value(direction)
+        
+        tick_us = (self.rmt.clock_div() / self.rmt.source_freq()) * 1e6
+        step_ticks = int(step_delay_us / (tick_us))
+        step_ticks = min(32767, (max(0, step_ticks)))
+        
         pulses = []
-        for _ in range(steps):
-            pulses.append(step_delay_us) 
-            pulses.append(step_delay_us - delay) 
-            if len(pulses) > 60:
+        for i in range(steps):
+            pulses.append(step_ticks) 
+            pulses.append(step_ticks) 
+            
+            if (i + 1) % batch_size == 0:
                 self.rmt.write_pulses(pulses, 0)
-                del pulses
+                while not self.rmt.wait_done(): pass
                 pulses = []
-        else: 
+        
+        if pulses: 
             self.rmt.write_pulses(pulses, 0)
+            while not self.rmt.wait_done(): pass
+            
         del pulses; gc.collect()
 
 
 def main():
     stepper = Stepper(dir_pin=15, en_pin=13, step_pin=14, invert_en=True, clock_div=80)
     with stepper:
-        stepper.move(-3000)
-        while not stepper.rmt.wait_done(): pass
+        stepper.move(-635, step_delay_us=500)
+        time.sleep(1)
+        stepper.move(635, step_delay_us=500)
         
 main()
