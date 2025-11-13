f33700c72e29f5c71c254cfa37d5917e07062b0a
denis | walderhu <denis.lipatov163@gmail.com>
2025-11-13 17:16:09 +0300

    косметика

--- src/master.mk
+++ src/master.mk
@@ -3,28 +3,15 @@ BOARD2 = 192.168.0.232
 CLI = /home/des/miniforge3/envs/esp/bin/python /home/des/WORK/src/tools/webrepl_cli.py
 PASS = 1234
 FILE = TCP.py
-# FILE = can4.py
-# FILE = can5.py
-# FILE = test_CAN.py
-# FILE = Bluetooth.py
 MODULE = $(basename $(FILE))
 
-
-# Сброс платы
 reset:
 	@$(CLI) -p $(PASS) $(BOARD2):/ -e "import machine; machine.soft_reset()" > /dev/null; sleep 0.5
 	@$(CLI) -p $(PASS) $(BOARD1):/ -e "import machine; machine.soft_reset()" > /dev/null; sleep 0.5
 
-# Основная сборка
 main: reset
 	@$(CLI) -p $(PASS) $(FILE) $(BOARD1):/$(FILE) ; sleep 0.5
 	@$(CLI) -p $(PASS) $(FILE) $(BOARD2):/$(FILE) ; sleep 0.5
 
 	@$(CLI) -p $(PASS) $(BOARD1):/ -e "import $(MODULE)" --verbose --exit
 	@$(CLI) -p $(PASS) $(BOARD2):/ -e "import $(MODULE)" --verbose --exit
-
-
-master:
-	@$(CLI) -p $(PASS) $(BOARD1);
-slave: 
-	@$(CLI) -p $(PASS) $(BOARD2);
