93f7a33749510797bd0d0ae997f5711be38be921
denis | walderhu <denis.lipatov163@gmail.com>
2025-11-21 01:40:42 +0300

    модификация сайта, в частности джостика добавление оси Z и хваталки

--- src/server/server.py
+++ src/server/server.py
@@ -128,15 +128,13 @@ def on_press(direction, value=None):
 
 # --- HTTP сервер ---
 def serve():
-    with open("index.html", "r", encoding="utf-8") as f:
-        HTML = f.read()
-
+    with open("index.html", "r", encoding="utf-8") as f: HTML = f.read()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     s.bind(("0.0.0.0", 8081))
     s.listen(1)
-    print("Открой http://localhost:8081")
+    # print("Открой http://localhost:8081")
     print("Открой https://jazlynn-englacial-undoubtfully.ngrok-free.dev/")
 
     while True:
@@ -150,32 +148,26 @@ def serve():
                     dir = data.get('dir')
                     action = data.get('action')
                     value = data.get('value', None)
-                    if action == 'press':
-                        on_press(dir, value)
-                except Exception as e:
-                    print("Ошибка JSON:", e)
+                    if action == 'press': on_press(dir, value)
+                except Exception as e: print("Ошибка JSON:", e)
                 cl.send(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nOK")
             elif req.startswith('GET /misc/'):
                 path = req.split(' ', 2)[1][1:]
                 try:
-                    with open(path, 'rb') as f:
-                        content = f.read()
+                    with open(path, 'rb') as f: content = f.read()
                     if path.endswith('.svg'): ctype = 'image/svg+xml'
                     elif path.endswith('.png'): ctype = 'image/png'
                     elif path.endswith('.jpg') or path.endswith('.jpeg'): ctype = 'image/jpeg'
                     else: ctype = 'application/octet-stream'
                     cl.send(f"HTTP/1.1 200 OK\r\nContent-Type: {ctype}\r\n\r\n".encode())
                     cl.sendall(content)
-                except:
-                    cl.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+                except: cl.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
             elif req.startswith('GET /style.css'):
                 try:
-                    with open('style.css', 'rb') as f:
-                        css = f.read()
+                    with open('style.css', 'rb') as f: css = f.read()
                     cl.send(b"HTTP/1.1 200 OK\r\nContent-Type: text/css; charset=utf-8\r\n\r\n")
                     cl.sendall(css)
-                except:
-                    cl.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+                except: cl.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
                     
             elif req.startswith('GET /coords'):
                 global X, Y
